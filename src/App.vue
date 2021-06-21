1c8797fb16beea6d75d05e81b8d46f03753773c3
francs2812 <emiliofrangella2812@gmail.com>
2021-06-21 15:57:37 +0200

    base + inizio header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Header />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&family=Noto+Serif&display=swap');
+  //font-family: 'Libre Baskerville', serif;
+  @import url('https://fonts.googleapis.com/css2?family=Lato:wght@700&family=Libre+Baskerville:wght@400;700&family=Noto+Serif&display=swap'); //font-family: 'Lato', sans-serif;
+
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,80 @@
+<template>
+  <header>
+        <nav>
+            <a href="#" class="logo">
+                <img src="../assets/img/logo-img-01.png" alt="">
+            </a>
+
+            <ul>
+                <li><a href="">homes</a></li>
+                <li><a href="">pages</a></li>
+                <li><a href="">blog</a></li>
+                <li><a href="">shop</a></li>
+                <li><a href="">events</a></li>
+                <li><a href="">elements</a></li>
+                <li><a href="">serch</a></li>
+                <li><a href="">menu</a></li>
+            </ul>
+
+        </nav>
+        <div id="shop_slide">
+            <div></div>
+        </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name:"Header"
+}
+</script>
+
+<style lang="scss" scoped>
+    header {
+        height: 100vh;
+        width: 100%;
+        background-image: url(../assets/img/rev-slider-main-home-img-03.jpg);
+        background-size: cover;
+        padding: 20px;
+        
+        nav {
+            display: flex;
+            align-items: center;
+            justify-content:space-between;
+
+            a {
+                height: 100%;
+
+                img {
+                   height: 100%; 
+                }
+            }
+
+            ul {
+                li {
+                    display: inline-block;
+                    margin: 10px;
+
+                    a {
+                        text-decoration: none;
+                        color: black;
+                        text-transform: uppercase;
+                        font-family: 'Lato', sans-serif;
+                        font-weight: 700;
+                    }
+
+                }
+            }
+
+        }
+        #shop_slide {
+            background-color: blue;
+            div {
+                display: block;
+                height: 300px;
+            }
+        }
+    }
+
+</style>
+
